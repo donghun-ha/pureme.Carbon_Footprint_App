374c81898cf72b32b2ac2f8a3fb9da5e5695d2bb
nnmmnn15 <shals1645@naver.com>
2024-10-07 11:18:06 +0900

    login UI

--- team_project2_pure_me/lib/view/login_signin/login.dart
+++ team_project2_pure_me/lib/view/login_signin/login.dart
@@ -1,17 +1,90 @@
 import 'package:flutter/material.dart';
 
 class Login extends StatelessWidget {
-  const Login({super.key});
+  Login({super.key});
+
+  final TextEditingController idController = TextEditingController();
+  final TextEditingController pwController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // 필요한 함수: 로그인 가능 확인
-      /// 또한, 매니저라면 매니지 홈으로 넘겨야함.
-      // 로그인 가능해서실제로 Home으로 넘어갈때 
-      // getStorage의 box에 id를 쓰자. 
-      //아이디는 getStoarge로 넘기자.
-      // 
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        // 필요한 함수: 로그인 가능 확인
+        /// 또한, 매니저라면 매니지 홈으로 넘겨야함.
+        // 로그인 가능해서실제로 Home으로 넘어갈때
+        // getStorage의 box에 id를 쓰자.
+        //아이디는 getStoarge로 넘기자.
+        //
+        body: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextField(
+                    controller: idController,
+                    decoration: InputDecoration(
+                      labelText: '아이디',
+                      hintText: 'pureme@example.com',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                        borderSide: const BorderSide(
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                  TextField(
+                    controller: pwController,
+                    decoration: InputDecoration(
+                      labelText: '비밀번호',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                        borderSide: const BorderSide(
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    obscureText: true,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text('이메일이 없으신가요?    '),
+                      GestureDetector(
+                        onTap: () {
+                          // 회원가입 이동
+                        },
+                        child: const Text(
+                          '회원가입',
+                          style: TextStyle(
+                            color: Colors.red,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFFEF9D11)),
+                      onPressed: () {
+                        // 로그인 로직
+                      },
+                      child: const Text('로그인'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
